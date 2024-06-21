28771dcaf16eeb587ec03c44ef4b0f514fe75293
wonjunYou <youwjune@gmail.com>
2024-06-21 18:38:03 +0900

    [IDLE-46] Swagger 셋업

--- idle-api/build.gradle.kts
+++ idle-api/build.gradle.kts
@@ -5,6 +5,7 @@ dependencies {
 
     implementation(libs.spring.boot.starter.web)
     implementation(libs.spring.boot.starter.data.jpa)
+    implementation(libs.springdoc.openapi.starter.webmvc.ui)
 
     developmentOnly(libs.spring.boot.devtools)
     developmentOnly(libs.spring.boot.docker.compose)
