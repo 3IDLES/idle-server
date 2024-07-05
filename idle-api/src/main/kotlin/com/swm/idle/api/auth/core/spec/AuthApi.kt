f1400aa99819df6f0c5ece69e2ef3300eff68b14
wonjunYou <youwjune@gmail.com>
2024-07-05 16:27:05 +0900

    [IDLE-000] SMS 인증 API 문서에 에러 명세 추가

--- idle-api/src/main/kotlin/com/swm/idle/api/auth/core/spec/AuthApi.kt
+++ idle-api/src/main/kotlin/com/swm/idle/api/auth/core/spec/AuthApi.kt
@@ -2,7 +2,12 @@ package com.swm.idle.api.auth.core.spec
 
 import com.swm.idle.api.auth.core.dto.ConfirmSmsVerificationRequest
 import com.swm.idle.api.auth.core.dto.SendSmsVerificationRequest
+import com.swm.idle.api.common.exception.ErrorResponse
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
@@ -10,13 +15,33 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 
-@Tag(name = "Auth-Common", description = "공통 Auth API")
+@Tag(name = "Auth-Core", description = "공통 Auth API")
 @RequestMapping("/api/v1/auth/core", produces = ["application/json"])
 interface AuthApi {
 
     @Operation(summary = "인증번호 메세지 발송 API")
     @PostMapping("/send")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "204",
+                description = "전송 성공",
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "전송 실패",
+                content = [
+                    Content(
+                        mediaType = "application/json",
+                        schema = Schema(
+                            implementation = ErrorResponse::class
+                        )
+                    ),
+                ]
+            ),
+        ]
+    )
     fun sendVerificationMessage(
         @RequestBody request: SendSmsVerificationRequest,
     )
@@ -24,6 +49,26 @@ interface AuthApi {
     @Operation(summary = "인증번호 메세지 검증 API")
     @PostMapping("/confirm")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "204",
+                description = "인증번호 검증 성공",
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "인증번호 검증 실패 / 인증번호가 존재하지 않음",
+                content = [
+                    Content(
+                        mediaType = "application/json",
+                        schema = Schema(
+                            implementation = ErrorResponse::class
+                        )
+                    ),
+                ]
+            ),
+        ]
+    )
     fun confirmVerificationMessage(
         @RequestBody request: ConfirmSmsVerificationRequest,
     )
