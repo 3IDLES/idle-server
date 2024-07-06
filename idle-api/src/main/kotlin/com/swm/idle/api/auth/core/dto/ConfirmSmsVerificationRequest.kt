ffccdcd4b346a2c2f1eb379335dc9218311527d9
wonjunYou <youwjune@gmail.com>
2024-07-06 16:34:13 +0900

    [IDLE-000] API 명세 수정

--- idle-api/src/main/kotlin/com/swm/idle/api/auth/core/dto/ConfirmSmsVerificationRequest.kt
+++ idle-api/src/main/kotlin/com/swm/idle/api/auth/core/dto/ConfirmSmsVerificationRequest.kt
@@ -7,9 +7,8 @@ import io.swagger.v3.oas.annotations.media.Schema
     description = "인증 번호 검증 요청"
 )
 data class ConfirmSmsVerificationRequest(
-    @Schema(description = "Phone Number", example = "010-0000-0000")
+    @Schema(description = "핸드폰 번호", example = "010-0000-0000")
     val phoneNumber: String,
-
-    @Schema(description = "Verification Number", example = "123456")
+    @Schema(description = "인증 번호", example = "123456")
     val verificationNumber: String,
 )
