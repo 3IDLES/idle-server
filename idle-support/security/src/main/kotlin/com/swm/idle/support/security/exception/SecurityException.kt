658f5460a21ac7ff51e2d63a207a78b19f97a810
wonjunYou <youwjune@gmail.com>
2024-07-10 16:52:07 +0900

    [IDLE-69] Security Exception 명세 및 별도 Handler 추가

--- idle-support/security/src/main/kotlin/com/swm/idle/support/security/exception/SecurityException.kt
+++ idle-support/security/src/main/kotlin/com/swm/idle/support/security/exception/SecurityException.kt
@@ -10,6 +10,12 @@ sealed class SecurityException(
     class UnAuthorizedRequest(message: String = "인증되지 않은 사용자입니다.") :
         SecurityException(codeNumber = 1, message = message)
 
+    class InvalidLoginRequest(message: String = "올바르지 않은 아이디 또는 비밀번호입니다.") :
+        SecurityException(codeNumber = 2, message = message)
+
+    class InvalidPassword(message: String = "올바르지 않은 비밀번호입니다.") :
+        SecurityException(codeNumber = 3, message = message)
+
     companion object {
         const val CODE_PREFIX = "SECURITY"
     }
