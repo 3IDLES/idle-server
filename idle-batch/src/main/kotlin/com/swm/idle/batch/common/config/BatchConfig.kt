d79bbc55d3ac0e04c0368695bf128a51759ca23f
wonjunYou <youwjune@gmail.com>
2024-08-07 20:20:40 +0900

    [IDLE-000] @EnableAutoConfiguration 삭제

--- idle-batch/src/main/kotlin/com/swm/idle/batch/common/config/BatchConfig.kt
+++ idle-batch/src/main/kotlin/com/swm/idle/batch/common/config/BatchConfig.kt
@@ -1,11 +1,9 @@
 package com.swm.idle.batch.common.config
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 @ComponentScan(basePackages = ["com.swm.idle.batch"])
-@EnableAutoConfiguration
 class BatchConfig {
 }
