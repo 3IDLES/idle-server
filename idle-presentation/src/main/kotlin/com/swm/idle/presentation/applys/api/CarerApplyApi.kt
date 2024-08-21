fd97233fe4cf5ab699dbeb1d154833ed6a7b7345
wonjunYou <youwjune@gmail.com>
2024-08-21 12:27:13 +0900

    [IDLE-000] 요양 보호사 지원 API path 수정

--- idle-presentation/src/main/kotlin/com/swm/idle/presentation/applys/api/CarerApplyApi.kt
+++ idle-presentation/src/main/kotlin/com/swm/idle/presentation/applys/api/CarerApplyApi.kt
@@ -10,13 +10,13 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 
-@Tag(name = "Applyment - Carer", description = "요양 보호사 공고 지원 API")
-@RequestMapping("/api/v1/applyments", produces = ["application/json;charset=utf-8"])
+@Tag(name = "Applys - Carer", description = "요양 보호사 공고 지원 API")
+@RequestMapping("/api/v1/applys", produces = ["application/json;charset=utf-8"])
 interface CarerApplyApi {
 
     @Secured
     @Operation(summary = "요양 보호사 공고 지원하기 API")
-    @PostMapping("/history")
+    @PostMapping
     @ResponseStatus(HttpStatus.OK)
     fun createApply(
         @RequestBody request: CreateApplyRequest,
