5270deb679d36bc4c46ed6a040b4d46531433914
wonjunYou <youwjune@gmail.com>
2024-09-11 15:59:00 +0900

    [IDLE-000] Batch Job Runner 커스텁 구현

--- idle-batch/src/main/kotlin/com/swm/idle/batch/common/scheduler/CrawlingJobScheduler.kt
+++ idle-batch/src/main/kotlin/com/swm/idle/batch/common/scheduler/CrawlingJobScheduler.kt
@@ -13,7 +13,7 @@ class CrawlingJobScheduler(
     private val crawlingJobConfig: CrawlingJobConfig,
 ) {
 
-    @Scheduled(cron = "0 0 6 * * *")
+    @Scheduled(cron = "0 5 * * * *")
     fun scheduleJob() {
         val jobParameters: JobParameters = JobParametersBuilder()
             .addLong("timestamp", System.currentTimeMillis())
