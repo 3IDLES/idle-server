58e7addc11435bf4c27c8b5b6a1239062c742c63
wonjunYou <youwjune@gmail.com>
2024-09-21 14:39:24 +0900

    [IDLE-000] 크롤링 기준시각 14:50으로 변경

--- idle-batch/src/main/kotlin/com/swm/idle/batch/common/scheduler/CrawlingJobScheduler.kt
+++ idle-batch/src/main/kotlin/com/swm/idle/batch/common/scheduler/CrawlingJobScheduler.kt
@@ -13,7 +13,7 @@ class CrawlingJobScheduler(
     private val crawlingJobConfig: CrawlingJobConfig,
 ) {
 
-    @Scheduled(cron = "0 15 14 * * *")
+    @Scheduled(cron = "0 50 14 * * *")
     fun scheduleJob() {
         val jobParameters: JobParameters = JobParametersBuilder()
             .addLong("timestamp", System.currentTimeMillis())
