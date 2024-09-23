945414a66968c7b7abf66a4d1868264f1966ea60
wonjunYou <youwjune@gmail.com>
2024-09-23 17:38:43 +0900

    [IDLE-389] 사업자 등록번호 client properties 클래스명 변경

--- idle-infrastructure/client/src/main/kotlin/com/swm/idle/infrastructure/client/businessregistration/properties/BusinessRegistrationNumberProperties.kt
+++ idle-infrastructure/client/src/main/kotlin/com/swm/idle/infrastructure/client/businessregistration/properties/BusinessRegistrationNumberProperties.kt
@@ -3,7 +3,7 @@ package com.swm.idle.infrastructure.client.businessregistration.properties
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties("business-registration-number")
-class ClientProperties(
+class BusinessRegistrationNumberProperties(
     val apikey: String,
     val path: String,
     val searchType: String,
