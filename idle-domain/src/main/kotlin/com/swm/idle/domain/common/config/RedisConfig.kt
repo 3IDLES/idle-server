1fce246fca4337675ab91030473857d0eec861b2
wonjunYou <youwjune@gmail.com>
2024-09-24 18:17:18 +0900

    [IDLE-000] Redis 비밀번호 설정 추가

--- idle-domain/src/main/kotlin/com/swm/idle/domain/common/config/RedisConfig.kt
+++ idle-domain/src/main/kotlin/com/swm/idle/domain/common/config/RedisConfig.kt
@@ -5,6 +5,8 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisPassword
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
@@ -18,7 +20,13 @@ class RedisConfig(
 
     @Bean
     fun connectionFactory(): RedisConnectionFactory {
-        return LettuceConnectionFactory(redisProperties.host, redisProperties.port)
+        RedisStandaloneConfiguration().apply {
+            hostName = redisProperties.host
+            port = redisProperties.port
+            password = RedisPassword.of(redisProperties.password)
+        }.also {
+            return LettuceConnectionFactory(it)
+        }
     }
 
     @Bean
