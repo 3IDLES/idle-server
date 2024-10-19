b751ac19d42c60e846ce2cba0cf8f8aa836f1f8c
wonjunYou <youwjune@gmail.com>
2024-10-19 12:42:30 +0900

    [IDLE-418] 알림 목록 조회 API

--- idle-application/src/main/kotlin/com/swm/idle/application/notification/facade/NotificationFacadeService.kt
+++ idle-application/src/main/kotlin/com/swm/idle/application/notification/facade/NotificationFacadeService.kt
@@ -2,7 +2,11 @@ package com.swm.idle.application.notification.facade
 
 import com.swm.idle.application.common.security.getUserAuthentication
 import com.swm.idle.application.notification.domain.NotificationService
+import com.swm.idle.application.notification.utils.converter.NotificationDetailsConverter
+import com.swm.idle.domain.common.dto.NotificationQueryDto
 import com.swm.idle.support.security.exception.SecurityException
+import com.swm.idle.support.transfer.jobposting.carer.CursorScrollRequest
+import com.swm.idle.support.transfer.notification.NotificationScrollResponse
 import com.swm.idle.support.transfer.notification.UnreadNotificationCountResponse
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -12,6 +16,7 @@ import java.util.*
 @Transactional(readOnly = true)
 class NotificationFacadeService(
     private val notificationService: NotificationService,
+    private val notificationDetailsConverter: NotificationDetailsConverter,
 ) {
 
     fun readNotification(notificationId: UUID) {
@@ -34,6 +39,57 @@ class NotificationFacadeService(
                 unreadNotificationCount = it
             )
         }
+    }
+
+    fun getNotifications(request: CursorScrollRequest): NotificationScrollResponse {
+        val userId = getUserAuthentication().userId
+
+        val (items, next) = scrollByUser(
+            userId = userId,
+            next = request.next,
+            limit = request.limit,
+        )
+
+        return NotificationScrollResponse.from(
+            items = items.map { notificationQueryDto ->
+                NotificationScrollResponse.NotificationDto(
+                    id = notificationQueryDto.id,
+                    isRead = notificationQueryDto.isRead,
+                    title = notificationQueryDto.title,
+                    body = notificationQueryDto.body,
+                    notificationType = notificationQueryDto.notificationType,
+                    createdAt = notificationQueryDto.createdAt,
+                    imageUrl = notificationQueryDto.imageUrl,
+                    notificationDetails = notificationDetailsConverter.convertToMap(
+                        notificationQueryDto.notificationDetails
+                    )  // map으로 변환
+                )
+            },
+            next = next,
+            total = items.size
+        )
+    }
+
+    private fun scrollByUser(
+        userId: UUID,
+        next: UUID?,
+        limit: Long,
+    ): Pair<List<NotificationQueryDto>, UUID?> {
+
+        val notificationDtos = notificationService.findAllByUserId(
+            next = next,
+            limit = limit + 1,
+            userId = userId,
+        )
+
+        val newNext =
+            if (notificationDtos.size > limit) notificationDtos.last().id else null
+        val items =
+            if (newNext == null) notificationDtos else notificationDtos.subList(
+                0,
+                limit.toInt()
+            )
+        return items to newNext
     }
 
 }
---
+++ idle-support/transfer/src/main/kotlin/com/swm/idle/support/transfer/notification/NotificationScrollResponse.kt
@@ -0,0 +1,49 @@
+package com.swm.idle.support.transfer.notification
+
+import com.swm.idle.domain.notification.enums.NotificationType
+import com.swm.idle.support.transfer.common.ScrollResponse
+import io.swagger.v3.oas.annotations.media.Schema
+import java.time.LocalDateTime
+import java.util.*
+
+@Schema(
+    name = "NotificationScrollResponse",
+    description = "알림 전체 조회 페이징 응답",
+)
+data class NotificationScrollResponse(
+    override val items: List<NotificationDto>,
+    override val next: UUID?,
+    override val total: Int,
+) : ScrollResponse<NotificationScrollResponse.NotificationDto, UUID?>(
+    items = items,
+    next = next,
+    total = total,
+) {
+
+    data class NotificationDto(
+        val id: UUID,
+        val isRead: Boolean,
+        val title: String,
+        val body: String,
+        val notificationType: NotificationType,
+        val createdAt: LocalDateTime,
+        val imageUrl: String?,
+        var notificationDetails: Map<String, Any>,
+    )
+
+    companion object {
+
+        fun from(
+            items: List<NotificationDto>,
+            next: UUID?,
+            total: Int,
+        ): NotificationScrollResponse {
+            return NotificationScrollResponse(
+                items = items,
+                next = next,
+                total = total,
+            )
+        }
+    }
+
+}
