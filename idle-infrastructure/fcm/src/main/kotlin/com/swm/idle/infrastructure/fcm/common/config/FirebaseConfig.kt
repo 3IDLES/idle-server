22669426e94400902cdb2fe1e67d85d9996e27b3
wonjunYou <youwjune@gmail.com>
2024-10-21 17:06:53 +0900

    [IDLE-000] firebase app 초기화 로직 주석 처리

--- idle-infrastructure/fcm/src/main/kotlin/com/swm/idle/infrastructure/fcm/common/config/FirebaseConfig.kt
+++ idle-infrastructure/fcm/src/main/kotlin/com/swm/idle/infrastructure/fcm/common/config/FirebaseConfig.kt
@@ -1,13 +1,9 @@
 package com.swm.idle.infrastructure.fcm.common.config
 
-import com.google.auth.oauth2.GoogleCredentials
-import com.google.firebase.FirebaseApp
-import com.google.firebase.FirebaseOptions
 import io.github.oshai.kotlinlogging.KotlinLogging
 import jakarta.annotation.PostConstruct
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.io.ClassPathResource
 
 @Configuration
 class FirebaseConfig(
@@ -17,17 +13,17 @@ class FirebaseConfig(
 
     private val logger = KotlinLogging.logger {}
 
-    @PostConstruct
-    fun initializeFirebaseApp() {
-        val googleCredentials =
-            GoogleCredentials.fromStream(ClassPathResource(firebaseConfigJsonPath).inputStream)
-
-        val fireBaseOptions = FirebaseOptions.builder()
-            .setCredentials(googleCredentials)
-            .build()
-
-        FirebaseApp.initializeApp(fireBaseOptions)
-    }
+//    @PostConstruct
+//    fun initializeFirebaseApp() {
+//        val googleCredentials =
+//            GoogleCredentials.fromStream(ClassPathResource(firebaseConfigJsonPath).inputStream)
+//
+//        val fireBaseOptions = FirebaseOptions.builder()
+//            .setCredentials(googleCredentials)
+//            .build()
+//
+//        FirebaseApp.initializeApp(fireBaseOptions)
+//    }
 
     @PostConstruct
     fun printWorkingDirectory() {
