146be64314646bda5a784dfe499ff149f0878002
wonjunYou <youwjune@gmail.com>
2024-10-21 17:12:19 +0900

    [IDLE-000] firebase app 초기화 로직 주석 처리

--- idle-infrastructure/fcm/src/main/kotlin/com/swm/idle/infrastructure/fcm/common/config/FirebaseConfig.kt
+++ idle-infrastructure/fcm/src/main/kotlin/com/swm/idle/infrastructure/fcm/common/config/FirebaseConfig.kt
@@ -1,34 +1,34 @@
-package com.swm.idle.infrastructure.fcm.common.config
-
-import io.github.oshai.kotlinlogging.KotlinLogging
-import jakarta.annotation.PostConstruct
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Configuration
-
-@Configuration
-class FirebaseConfig(
-    @Value("\${firebase.json.path}")
-    var firebaseConfigJsonPath: String,
-) {
-
-    private val logger = KotlinLogging.logger {}
-
+//package com.swm.idle.infrastructure.fcm.common.config
+//
+//import io.github.oshai.kotlinlogging.KotlinLogging
+//import jakarta.annotation.PostConstruct
+//import org.springframework.beans.factory.annotation.Value
+//import org.springframework.context.annotation.Configuration
+//
+//@Configuration
+//class FirebaseConfig(
+//    @Value("\${firebase.json.path}")
+//    var firebaseConfigJsonPath: String,
+//) {
+//
+//    private val logger = KotlinLogging.logger {}
+//
+////    @PostConstruct
+////    fun initializeFirebaseApp() {
+////        val googleCredentials =
+////            GoogleCredentials.fromStream(ClassPathResource(firebaseConfigJsonPath).inputStream)
+////
+////        val fireBaseOptions = FirebaseOptions.builder()
+////            .setCredentials(googleCredentials)
+////            .build()
+////
+////        FirebaseApp.initializeApp(fireBaseOptions)
+////    }
+//
 //    @PostConstruct
-//    fun initializeFirebaseApp() {
-//        val googleCredentials =
-//            GoogleCredentials.fromStream(ClassPathResource(firebaseConfigJsonPath).inputStream)
-//
-//        val fireBaseOptions = FirebaseOptions.builder()
-//            .setCredentials(googleCredentials)
-//            .build()
-//
-//        FirebaseApp.initializeApp(fireBaseOptions)
+//    fun printWorkingDirectory() {
+//        logger.warn { "Current working directory: " + System.getProperty("user.dir") }
 //    }
-
-    @PostConstruct
-    fun printWorkingDirectory() {
-        logger.warn { "Current working directory: " + System.getProperty("user.dir") }
-    }
-
-
-}
+//
+//
+//}
