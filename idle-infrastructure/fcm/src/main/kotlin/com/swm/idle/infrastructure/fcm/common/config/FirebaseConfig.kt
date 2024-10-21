24c516d1dc58d36aa652bc320a50a1a66624694a
wonjunYou <youwjune@gmail.com>
2024-10-21 18:03:17 +0900

    [IDLE-000] file 대신 string으로 주입받아 초기화하는 방식으로 전환

--- idle-infrastructure/fcm/src/main/kotlin/com/swm/idle/infrastructure/fcm/common/config/FirebaseConfig.kt
+++ idle-infrastructure/fcm/src/main/kotlin/com/swm/idle/infrastructure/fcm/common/config/FirebaseConfig.kt
@@ -1,33 +1,39 @@
 package com.swm.idle.infrastructure.fcm.common.config
 
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
 import io.github.oshai.kotlinlogging.KotlinLogging
 import jakarta.annotation.PostConstruct
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
+import java.io.ByteArrayInputStream
 
 @Configuration
 class FirebaseConfig(
-//    @Value("\${firebase.json.path}")
-//    var firebaseConfigJsonPath: String,
+    @Value("\${firebase.json}")
+    var firebaseJsonString: String,
 ) {
 
     private val logger = KotlinLogging.logger {}
 
-//    @PostConstruct
-//    fun initializeFirebaseApp() {
-//        val googleCredentials =
-//            GoogleCredentials.fromStream(ClassPathResource(firebaseConfigJsonPath).inputStream)
-//
-//        val fireBaseOptions = FirebaseOptions.builder()
-//            .setCredentials(googleCredentials)
-//            .build()
-//
-//        FirebaseApp.initializeApp(fireBaseOptions)
-//    }
-
     @PostConstruct
-    fun printWorkingDirectory() {
-        logger.warn { "Current working directory: " + System.getProperty("user.dir") }
-    }
+    fun initializeFirebaseApp() {
+        try {
+            val inputStream = ByteArrayInputStream(firebaseJsonString.toByteArray(Charsets.UTF_8))
 
+            val googleCredentials = GoogleCredentials.fromStream(inputStream)
+
+            val fireBaseOptions = FirebaseOptions.builder()
+                .setCredentials(googleCredentials)
+                .build()
+
+            FirebaseApp.initializeApp(fireBaseOptions)
+
+            logger.info { "FirebaseApp has been initialized successfully." }
+        } catch (e: Exception) {
+            logger.error(e) { "Error initializing FirebaseApp." }
+        }
+    }
 
 }
