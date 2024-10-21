6588a7d9565c48206fe1f8104f583bcb34565ed8
wonjunYou <youwjune@gmail.com>
2024-10-21 13:58:13 +0900

    [IDLE-000] firebaseApp 초기화 임시 비활성화

--- idle-infrastructure/fcm/src/main/kotlin/com/swm/idle/infrastructure/fcm/common/config/FirebaseConfig.kt
+++ idle-infrastructure/fcm/src/main/kotlin/com/swm/idle/infrastructure/fcm/common/config/FirebaseConfig.kt
@@ -1,27 +1,27 @@
-package com.swm.idle.infrastructure.fcm.common.config
-
-import com.google.auth.oauth2.GoogleCredentials
-import com.google.firebase.FirebaseApp
-import com.google.firebase.FirebaseOptions
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Configuration
-import org.springframework.core.io.ClassPathResource
-
-@Configuration
-class FirebaseConfig(
-    @Value("\${firebase.json.path}")
-    var firebaseConfigJsonPath: String,
-) {
-
-    fun initializeFirebaseApp() {
-        val googleCredentials =
-            GoogleCredentials.fromStream(ClassPathResource(firebaseConfigJsonPath).inputStream)
-
-        val fireBaseOptions = FirebaseOptions.builder()
-            .setCredentials(googleCredentials)
-            .build()
-
-        FirebaseApp.initializeApp(fireBaseOptions)
-    }
-
-}
+//package com.swm.idle.infrastructure.fcm.common.config
+//
+//import com.google.auth.oauth2.GoogleCredentials
+//import com.google.firebase.FirebaseApp
+//import com.google.firebase.FirebaseOptions
+//import org.springframework.beans.factory.annotation.Value
+//import org.springframework.context.annotation.Configuration
+//import org.springframework.core.io.ClassPathResource
+//
+//@Configuration
+//class FirebaseConfig(
+//    @Value("\${firebase.json.path}")
+//    var firebaseConfigJsonPath: String,
+//) {
+//
+//    fun initializeFirebaseApp() {
+//        val googleCredentials =
+//            GoogleCredentials.fromStream(ClassPathResource(firebaseConfigJsonPath).inputStream)
+//
+//        val fireBaseOptions = FirebaseOptions.builder()
+//            .setCredentials(googleCredentials)
+//            .build()
+//
+//        FirebaseApp.initializeApp(fireBaseOptions)
+//    }
+//
+//}
