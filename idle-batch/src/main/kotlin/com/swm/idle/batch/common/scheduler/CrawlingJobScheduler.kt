1a63892d46a455794ccdf5df295a7a405328adc6
wonjunYou <youwjune@gmail.com>
2024-10-23 17:00:26 +0900

    [IDLE-000] 크롤링 주기 하루 2회로 변경

--- idle-batch/src/main/kotlin/com/swm/idle/batch/common/scheduler/CrawlingJobScheduler.kt
+++ idle-batch/src/main/kotlin/com/swm/idle/batch/common/scheduler/CrawlingJobScheduler.kt
@@ -13,7 +13,7 @@ class CrawlingJobScheduler(
     private val crawlingJobConfig: CrawlingJobConfig,
 ) {
 
-    @Scheduled(cron = "0 00 16 * * *")
+    @Scheduled(cron = "0 0 1,13 * * *")
     fun scheduleJob() {
         val jobParameters: JobParameters = JobParametersBuilder()
             .addLong("timestamp", System.currentTimeMillis())
