6f6ff7a2a1ab8a462694db5ef04d3d172e8bc398
wonjunYou <youwjune@gmail.com>
2024-10-25 16:51:37 +0900

    [IDLE-000] deviceToken 서비스에서 트랜잭션 어노테이션 추가

--- idle-application/src/main/kotlin/com/swm/idle/application/notification/facade/DeviceTokenFacadeService.kt
+++ idle-application/src/main/kotlin/com/swm/idle/application/notification/facade/DeviceTokenFacadeService.kt
@@ -4,12 +4,15 @@ import com.swm.idle.application.common.security.getUserAuthentication
 import com.swm.idle.application.notification.domain.DeviceTokenService
 import com.swm.idle.domain.user.common.enum.UserType
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
+@Transactional(readOnly = true)
 class DeviceTokenFacadeService(
     private val deviceTokenService: DeviceTokenService,
 ) {
 
+    @Transactional
     fun createDeviceToken(deviceToken: String, userType: UserType) {
         val userId = getUserAuthentication().userId
         deviceTokenService.findByDeviceToken(deviceToken)?.let {
@@ -23,6 +26,7 @@ class DeviceTokenFacadeService(
         )
     }
 
+    @Transactional
     fun deleteDeviceToken(deviceToken: String) {
         deviceTokenService.deleteByDeviceToken(deviceToken)
     }
