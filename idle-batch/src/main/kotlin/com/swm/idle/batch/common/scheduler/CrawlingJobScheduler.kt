59b64af53d5e46c0c6bfea02d53ee0170103320b
wonjunYou <youwjune@gmail.com>
2024-10-25 16:59:32 +0900

    [IDLE-000] batch 테스트를 위해 30분마다 동작하도록 변경

--- idle-batch/src/main/kotlin/com/swm/idle/batch/common/scheduler/CrawlingJobScheduler.kt
+++ idle-batch/src/main/kotlin/com/swm/idle/batch/common/scheduler/CrawlingJobScheduler.kt
@@ -13,7 +13,7 @@ class CrawlingJobScheduler(
     private val crawlingJobConfig: CrawlingJobConfig,
 ) {
 
-    @Scheduled(cron = "0 0 1,13 * * *")
+    @Scheduled(cron = "0 0/30 * * * *")
     fun scheduleJob() {
         val jobParameters: JobParameters = JobParametersBuilder()
             .addLong("timestamp", System.currentTimeMillis())
