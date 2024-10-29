e1eb68acd509d8ce9247acb414010f800d85d619
wonjunYou <youwjune@gmail.com>
2024-10-29 18:48:21 +0900

    [IDLE-475] 채팅 메세지 최소, 최대 길이 제한 설정

--- idle-domain/src/main/kotlin/com/swm/idle/domain/chat/vo/Content.kt
+++ idle-domain/src/main/kotlin/com/swm/idle/domain/chat/vo/Content.kt
@@ -5,4 +5,15 @@ import com.swm.idle.domain.chat.enums.ContentType
 data class Content(
     val type: ContentType,
     val value: String,
-)
+) {
+
+    init {
+        require(value.isBlank()) { "채팅 메세지는 비어 있을 수 없습니다. " }
+        require(value.length <= MAXIMUM_CHAT_MESSAGE_LENGTH) { "채팅 메세지 최대 길이는 1,000자 입니다." }
+    }
+
+    companion object {
+
+        const val MAXIMUM_CHAT_MESSAGE_LENGTH = 1_000
+    }
+}
