6e6ef04b9e45278e8879276edd69ada9f7abec97
wonjunYou <youwjune@gmail.com>
2024-11-04 14:16:28 +0900

    [IDLE-493] flyway 스크립트 오탈자 수정

--- idle-domain/src/main/resources/db/migration/V4__alter_table_notification_add_column_device_token.sql
+++ idle-domain/src/main/resources/db/migration/V4__alter_table_notification_add_column_device_token.sql
@@ -1,4 +1,4 @@
 -- V4__alter_table_notification_add_column_device_token.sql
 
 -- Add secondary index on deviceToken and userId columns
-CREATE INDEX idx_user_id ON notification(userId);
+CREATE INDEX idx_user_id ON notification(user_id);
