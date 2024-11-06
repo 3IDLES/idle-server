53326c8677f5da1a5313f05e3634a815c437de7a
Wonjun You <59856002+wonjunYou@users.noreply.github.com>
2024-11-06 11:26:19 +0900

    [IDLE-000] 크롤링 시각 임시 변경

--- idle-batch/src/main/kotlin/com/swm/idle/batch/common/scheduler/CrawlingJobScheduler.kt
+++ idle-batch/src/main/kotlin/com/swm/idle/batch/common/scheduler/CrawlingJobScheduler.kt
@@ -13,7 +13,7 @@ class CrawlingJobScheduler(
     private val crawlingJobConfig: CrawlingJobConfig,
 ) {
 
-    @Scheduled(cron = "0 30 15 * * *")
+    @Scheduled(cron = "0 00 12 * * *")
     fun scheduleJob() {
         val jobParameters: JobParameters = JobParametersBuilder()
             .addLong("timestamp", System.currentTimeMillis())
