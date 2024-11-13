e5cb820ab5ca27599c2020f34c8e9c01e532b2ef
wonjunYou <youwjune@gmail.com>
2024-11-13 14:27:43 +0900

    [IDLE-000] 크롤링 중에 에러가 발생하더라도, 크롤링된 값은 무조건 반환하도록 변경

--- idle-batch/src/main/kotlin/com/swm/idle/batch/common/scheduler/CrawlingJobScheduler.kt
+++ idle-batch/src/main/kotlin/com/swm/idle/batch/common/scheduler/CrawlingJobScheduler.kt
@@ -13,7 +13,7 @@ class CrawlingJobScheduler(
     private val crawlingJobConfig: CrawlingJobConfig,
 ) {
 
-    @Scheduled(cron = "0 25 10 * * *")
+    @Scheduled(cron = "0 40 14 * * *")
     fun scheduleJob() {
         val jobParameters: JobParameters = JobParametersBuilder()
             .addLong("timestamp", System.currentTimeMillis())
