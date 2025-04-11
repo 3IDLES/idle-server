6c8e352867575034a6afc076f875686409407491
김명준 <86913355+mjj111@users.noreply.github.com>
2025-04-11 21:40:59 +0900

    [IDLE-561] Websocket payload 역직렬화를 위한 Json애너테이션 추가

--- idle-support/transfer/src/main/kotlin/com/swm/idle/support/transfer/chat/ReadChatMessagesReqeust.kt
+++ idle-support/transfer/src/main/kotlin/com/swm/idle/support/transfer/chat/ReadChatMessagesReqeust.kt
@@ -1,5 +1,10 @@
 package com.swm.idle.support.transfer.chat
 
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
 import java.util.UUID
 
-data class ReadChatMessagesReqeust(val chatroomId: UUID, val opponentId: UUID)
+data class ReadChatMessagesReqeust @JsonCreator constructor(
+    @JsonProperty("chatroomId") val chatroomId: UUID,
+    @JsonProperty("opponentId")val opponentId: UUID
+)
