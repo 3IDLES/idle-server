3b4cf466f5cab00d1175d7bb35a2bd432f5a9796
김명준 <86913355+mjj111@users.noreply.github.com>
2025-04-25 15:48:04 +0900

    [IDLE-568] pageIndex 초기화 오류 수정

--- idle-batch/src/main/kotlin/com/swm/idle/batch/step/PostingReader.kt
+++ idle-batch/src/main/kotlin/com/swm/idle/batch/step/PostingReader.kt
@@ -1,46 +1,30 @@
 package com.swm.idle.batch.step
 
 import com.swm.idle.batch.common.dto.CrawledJobPostingDto
-import com.swm.idle.batch.crawler.CrawlerConsts
-import com.swm.idle.batch.crawler.WorknetPageCrawler
-import com.swm.idle.batch.crawler.WorknetPostCrawler
+import com.swm.idle.batch.crawler.WorknetPageParser
+import com.swm.idle.batch.crawler.WorknetPostParser
 import org.springframework.batch.item.ItemStreamReader
 import java.util.concurrent.atomic.AtomicInteger
 
-class PostingReader : ItemStreamReader<List<CrawledJobPostingDto>> {
-    var crawlingUrl: String = ""
-    var postingCount: Int = 1
-    var lastPageJobPostingCount: Int = 1
-    var pageCount: Int = 1
-
-    init {
-        WorknetPageCrawler().initCounts(this)
-    }
+class PostingReader: ItemStreamReader<List<CrawledJobPostingDto>> {
+    val pageParser: WorknetPageParser = WorknetPageParser()
 
     companion object {
-        var nextPage = AtomicInteger(1)
+        var pageIndex = AtomicInteger(1)
     }
 
     override fun read(): List<CrawledJobPostingDto>? {
-        val currentPage = nextPage.getAndIncrement()
-        if (currentPage > pageCount) return null
+        val accessIndex = pageIndex.getAndIncrement()
 
-        val amount = getAmount(currentPage)
-        val nowUrl = getPageUrl(currentPage)
-
-        return WorknetPostCrawler().crawlPosts(amount, nowUrl)
-    }
-
-    private fun getAmount(currentPage: Int): Int {
-        if (currentPage == pageCount && lastPageJobPostingCount > 0) {
-            return lastPageJobPostingCount
+        if (pageParser.isOverPage(accessIndex)) {
+            pageIndex = AtomicInteger(1)
+            return null
         }
-        return CrawlerConsts.JOB_POSTING_COUNT_PER_PAGE.getIntValue()
-    }
 
-    private fun getPageUrl(currentPage: Int): String {
-        return crawlingUrl.replace(
-            Regex("pageIndex=\\d+"),
-            "pageIndex=$currentPage")
+        val fetchCount = pageParser.getFetchCount(accessIndex)
+        val accessURL = pageParser.getAccessURL(accessIndex)
+        val posts = WorknetPostParser().crawlPosts(fetchCount, accessURL)
+
+        return posts
     }
 }
