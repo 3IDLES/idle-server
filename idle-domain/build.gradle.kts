8f55ea50bdc34fa3bd94fb665035dbdf78600262
김명준 <86913355+mjj111@users.noreply.github.com>
2025-05-17 00:16:09 +0900

    [IDLE-573] 테스트 추가

--- idle-domain/build.gradle.kts
+++ idle-domain/build.gradle.kts
@@ -10,13 +10,20 @@ dependencies {
     implementation(project(":idle-support:common"))
     implementation(project(":idle-support:security"))
 
+    implementation("org.springframework.boot:spring-boot-starter-json")
     implementation(libs.spring.boot.starter.data.jpa)
     implementation(libs.spring.boot.starter.data.redis)
     implementation(libs.querydsl.spatial)
     implementation(libs.hibernate.spatial)
     implementation(libs.flyway.core)
     implementation(libs.flyway.mysql)
-
     runtimeOnly(libs.mysql.connector.java)
 
+    testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
+    testImplementation("io.kotest:kotest-assertions-core:5.8.0")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+
+    testImplementation("org.testcontainers:junit-jupiter:1.18.3")
+    testImplementation("com.h2database:h2")
+    testImplementation("it.ozimov:embedded-redis:0.7.2")
 }
