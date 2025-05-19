65dab3401e73ee18be5c46b35a1624f2c187f201
김명준 <86913355+mjj111@users.noreply.github.com>
2025-05-19 19:49:33 +0900

    [IDLE-576] Domain 모듈 테스트 작성을 위한 설정

---
+++ idle-domain/src/test/kotlin/com/swm/idle/config/DomainTestApplication.kt
@@ -0,0 +1,6 @@
+package com.swm.idle.config
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication(scanBasePackages = ["com.swm.idle.domain"])
+class DomainTestApplication
