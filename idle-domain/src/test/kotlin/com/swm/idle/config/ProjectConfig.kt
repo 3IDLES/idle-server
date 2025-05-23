1d8fca3a6eb1da4e1ca0605834bd8500277e0c87
김명준 <86913355+mjj111@users.noreply.github.com>
2025-05-23 16:56:33 +0900

    [IDLE-577] 사용하지 않는 파일 삭제

--- idle-domain/src/test/kotlin/com/swm/idle/config/ProjectConfig.kt
+++
@@ -1,9 +0,0 @@
-package com.swm.idle.config
-
-import io.kotest.core.config.AbstractProjectConfig
-import io.kotest.core.extensions.Extension
-import io.kotest.extensions.spring.SpringExtension
-
-object ProjectConfig : AbstractProjectConfig() {
-    override fun extensions(): List<Extension> = listOf(SpringExtension)
-}
