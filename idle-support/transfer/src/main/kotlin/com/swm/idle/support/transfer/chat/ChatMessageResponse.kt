65242a3cb4736891086189cd9e61e06a3c6c3146
김명준 <86913355+mjj111@users.noreply.github.com>
2025-05-23 17:53:16 +0900

    [IDLE-582] Send 요청시 사용자의 Redis chatroomid 에서 Sequence 가져와 메시지와 함께 Mysql에 저장

--- idle-support/transfer/src/main/kotlin/com/swm/idle/support/transfer/chat/ChatMessageResponse.kt
+++ idle-support/transfer/src/main/kotlin/com/swm/idle/support/transfer/chat/ChatMessageResponse.kt
@@ -1,27 +1,7 @@
 package com.swm.idle.support.transfer.chat
 
-import com.swm.idle.domain.chat.entity.jpa.ChatMessage
-import java.util.UUID
-import java.time.LocalDateTime
 
 data class ChatMessageResponse(
-    val type: ChatMessageType,
-    val id: UUID,
-    val chatroomId: UUID,
-    val senderId: UUID,
-    val receiverId: UUID,
-    val content: String,
-    val createdAt: LocalDateTime,
-    val isRead: Boolean
-) {
-    constructor(message: ChatMessage) : this(
-        type = ChatMessageType.MESSAGE,
-        id = message.id,
-        chatroomId = message.chatRoomId,
-        senderId = message.senderId,
-        receiverId = message.receiverId,
-        content = message.content,
-        isRead = message.isRead,
-        createdAt = message.createdAt ?: LocalDateTime.now(),
-    )
-}
+    val chatMessageInfos: List<ChatMessageInfo>,
+    val opponentSequence: Long
+)
