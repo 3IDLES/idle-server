4de2d8905c951f1fd09c511e3364faf29c892bac
김명준 <86913355+mjj111@users.noreply.github.com>
2025-05-23 17:42:50 +0900

    [IDLE-579]  사용자 현재 위치로 Redis에서 검색 후 ID 획득하도록 수정

---
+++ idle-domain/src/main/kotlin/com/swm/idle/domain/jobposting/repository/redis/RedisJobPostingRepository.kt
@@ -0,0 +1,101 @@
+package com.swm.idle.domain.jobposting.repository.redis
+
+import org.locationtech.jts.geom.Point
+import org.springframework.data.geo.Circle
+import org.springframework.data.geo.Distance
+import org.springframework.data.geo.GeoResults
+import org.springframework.data.geo.Metrics
+import org.springframework.data.redis.connection.RedisGeoCommands.GeoLocation
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.stereotype.Repository
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+@Repository
+class RedisJobPostingRepository(
+    private val redisTemplate: RedisTemplate<String, String>,
+) {
+    fun findByLocationAndDistance(
+        location: Point,
+        distance: Long,
+        limit: Long,
+        next: UUID?
+    ): List<UUID> {
+        val keys = makeRedisKeys()
+        val circle = Circle(
+            org.springframework.data.geo.Point(location.x, location.y),
+            Distance(distance.toDouble(),
+                Metrics.KILOMETERS)
+        )
+
+        if(next == null) {//first
+            val ids = findPostingIds(keys, circle)
+            return ids.take(limit.toInt())
+        }
+
+        return getPagedGeoUuids(keys, circle, next, limit)
+    }
+
+    private fun makeRedisKeys(): List<String> {
+        val today = LocalDate.now()
+        val keys = (0..12).map { offset ->
+            val date = today.minusDays(offset.toLong())
+            "job_postings_geo_${date.format(DateTimeFormatter.BASIC_ISO_DATE)}"
+        }
+        return keys
+    }
+
+    private fun findPostingIds(
+        keys: List<String>,
+        circle: Circle
+    ): MutableList<UUID> {
+        val ids = mutableListOf<UUID>()
+
+        keys.forEach { key ->
+            val geoResults = redisTemplate.opsForGeo().radius(key, circle) as GeoResults<GeoLocation<String>>
+
+            val uuids = geoResults.content.mapNotNull { geoLocation ->
+                UUID.fromString(geoLocation.content.name)
+            }
+            ids.addAll(uuids)
+        }
+        return ids
+    }
+
+    fun getPagedGeoUuids(
+        keys: List<String>,
+        circle: Circle,
+        next: UUID,
+        limit: Long
+    ): List<UUID> {
+        val distanceMap = mutableListOf<Pair<UUID, Double>>()
+
+        keys.forEach { key ->
+            val geoResults = redisTemplate.opsForGeo().radius(key, circle) as GeoResults<GeoLocation<String>>
+            geoResults.content.forEach { geoLocation ->
+                val uuid = runCatching { UUID.fromString(geoLocation.content.name) }.getOrNull()
+                val distance = geoLocation.distance?.value
+                if (uuid != null && distance != null) {
+                    distanceMap.add(uuid to distance)
+                }
+            }
+        }
+
+        val sortedIdsByDistance = distanceMap.sortedBy { it.second }.map { it.first }
+
+        val result = mutableListOf<UUID>()
+        var foundNext = false
+
+        for (id in sortedIdsByDistance) {
+            if (foundNext) {
+                result.add(id)
+                if (result.size == limit.toInt()) break
+            }
+
+            if (id == next) foundNext = true
+        }
+
+        return result
+    }
+}
